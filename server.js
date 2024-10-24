a85482d3a85f49285a99d46d5f2dbd34ebcdc62a
Erick Pajares <gunminiho@gmail.com>
2024-10-24 18:26:06 -0500

    backend con modelos terminados

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const tareajeRutas = require("./routes/index")
 const { PORT_TAREAJE } = process.env;
 
 server.use(express.json());
-server.use("/tareaje",tareajeRutas);
+server.use("/tareaje", tareajeRutas);
 
 server.get("/", (req, res) => {
     res.json({ hola: "Hello World" });
@@ -15,7 +15,7 @@ server.get("/", (req, res) => {
 
 server.listen(PORT_TAREAJE, () => {
     console.log(`TAREAJE: Server is running on port ${PORT_TAREAJE}`);
-    sequelize.sync({ alter: true }).then(
+    sequelize.sync({ force: true }).then(
         console.log("Database is connected")
     );
 });
