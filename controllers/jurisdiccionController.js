bb14b28630511fe7be0e66631effedbd2379fb88
vieri1 <vieriflores2002xdxd@gmail.com>
2024-10-25 15:48:02 -0500

    Se agrego y modifico los controladores regimenlaboral,turno,jurdicion

--- controllers/jurisdiccionController.js
+++ controllers/jurisdiccionController.js
@@ -1,1 +1,77 @@
-const Jurisdiccion=require('sequelize');
+const Jurisdiccion= require('../models/Jurisdiccions');
+const {sequelize}=require('../db_connection');
+//Trae todas las Jurisdicciones
+const getJurisdicciones=async () => {
+    try {
+        const response=await Jurisdiccion(sequelize).findAll({where: {
+            state:true  
+        }});
+        return response || null
+    } catch (error) {
+        console.error('Error al Obtener todas las Jurisdicciones ',error);
+        return false
+    }
+}
+
+//trae una Jurisdiccion especifica por id
+const getJurisdiccion = async (id) => {
+    try {
+        const Jurisdiccion = await Jurisdiccion(sequelize).findOne({where: {
+            id ,
+            state:true
+        }});
+        return Jurisdiccion || null;
+    } catch (error) {
+        console.error(`Error al obtener la Jurisdiccion: ${error.message}`);
+      return false
+    }
+};
+//Crea una nueva Jurisdiccion
+const createJurisdiccion = async ({nombre}) => {
+    try {
+        const Jurisdiccion = await Jurisdiccion(sequelize).create({ nombre });
+        return Jurisdiccion 
+    } catch (error) {
+        console.error('Error al crear una nueva Jurisdiccion',error)
+        return false
+    }
+};
+//elimina la Jurisdiccion o canbia el estado
+const deleteJurisdiccion = async (id) => {
+    try {
+        const Jurisdiccion = await Jurisdiccion(sequelize).findByPk(id);
+        Jurisdiccion.state = false;
+        await Jurisdiccion.save();
+       return Jurisdiccion || null
+    } catch (error) {
+        console.error('Error al canbiar de estado al eliminar Jurisdiccion');
+        return false;
+    }
+};
+
+
+const updateJurisdiccion = async (id, nuevaJurisdiccion) => {
+    if (id && nuevaJurisdiccion)
+        try {
+            const Jurisdiccion = await Jurisdiccion(sequelize).findOne({ where: { id } });
+            if (Jurisdiccion) 
+                await Jurisdiccion.update(nuevaJurisdiccion);
+                return Jurisdiccion || null;
+            
+        } catch (error) {
+            console.error('Error al actualizar la Jurisdiccion:', error.message);
+            return false;
+        }
+    else
+        return false;
+};  
+
+
+
+module.exports = {
+    getJurisdicciones,
+    createJurisdiccion,
+    getJurisdiccion,
+    updateJurisdiccion,
+    deleteJurisdiccion
+};
---
+++ controllers/turnoController.js
@@ -0,0 +1,77 @@
+const Turno= require('../models/Turnos');
+const {sequelize}=require('../db_connection');
+//Trae todas las Turnoes
+const getTurnos=async () => {
+    try {
+        const response=await Turno(sequelize).findAll({where: {
+            state:true  
+        }});
+        return response || null
+    } catch (error) {
+        console.error('Error al Obtener todas las Turnoes ',error);
+        return false
+    }
+}
+
+//trae una Turno especifica por id
+const getTurno = async (id) => {
+    try {
+        const Turno = await Turno(sequelize).findOne({where: {
+            id ,
+            state:true
+        }});
+        return Turno || null;
+    } catch (error) {
+        console.error(`Error al obtener la Turno: ${error.message}`);
+      return false
+    }
+};
+//Crea una nueva Turno
+const createTurno = async ({nombre}) => {
+    try {
+        const Turno = await Turno(sequelize).create({ nombre });
+        return Turno 
+    } catch (error) {
+        console.error('Error al crear una nueva Turno',error)
+        return false
+    }
+};
+//elimina la Turno o canbia el estado
+const deleteTurno = async (id) => {
+    try {
+        const Turno = await Turno(sequelize).findByPk(id);
+        Turno.state = false;
+        await Turno.save();
+       return Turno || null
+    } catch (error) {
+        console.error('Error al canbiar de estado al eliminar Turno');
+        return false;
+    }
+};
+
+
+const updateTurno = async (id, nuevaTurno) => {
+    if (id && nuevaTurno)
+        try {
+            const Turno = await getTurno(id);
+            if (Turno) 
+                await Turno.update(nuevaTurno);
+                return Turno || null;
+            
+        } catch (error) {
+            console.error('Error al actualizar la Turno:', error.message);
+            return false;
+        }
+    else
+        return false;
+};  
+
+
+
+module.exports = {
+    getTurnos,
+    createTurno,
+    getTurno,
+    updateTurno,
+    deleteTurno
+};
