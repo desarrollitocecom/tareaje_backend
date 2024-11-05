6b804af992f318e4a1a8d6a0d984c8946679b5a0
EduardoVillegasB02 <eduvb02@gmail.com>
2024-11-05 12:05:46 -0500

    Actualizacion Asistencia RangoHorario

--- handlers/rangohorarioHandlers.js
+++ handlers/rangohorarioHandlers.js
@@ -39,9 +39,22 @@ const getAllRangosHorariosHandler = async (req, res) => {
 
 // Handler para crear un nuevo rango horario
 const createRangoHorarioHandler = async (req, res) => {
-    const rangoHorarioData = req.body;
+    const { inicio, fin, id_cargo, id_turno} = req.body;
+    const errores = [];
+    const respuestaFalta = 'Es necesario que exista el parámetro'
+    if(!inicio) errores.push(`${respuestaFalta} FECHA INICIO`);
+    if(!fin) errores.push(`${respuestaFalta} FECHA FIN`);
+    if(!id_cargo) errores.push(`${respuestaFalta} ID CARGO`);
+    if(!id_turno) errores.push(`${respuestaFalta} ID TURNO`);
+    if(!/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/.test(inicio)) errores.push('El formato para FECHA INICIO es incorrecto');
+    if(!/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/.test(fin)) errores.push('El formato para FECHA FIN es incorrecto');
+    if(isNaN(id_cargo)) errores.push('El ID CARGO debe ser un entero');
+    if(isNaN(id_turno)) errores.push('El ID TURNO debe ser un entero');
+    if(errores.length > 0){
+        return res.status(400).json({ errores });
+    }
     try {
-        const newRangoHorario = await createRangoHorario(rangoHorarioData);
+        const newRangoHorario = await createRangoHorario(inicio, fin, id_cargo, id_turno);
         return res.status(200).json(newRangoHorario);
     } catch (error) {
         return res.status(500).json({
@@ -51,36 +64,54 @@ const createRangoHorarioHandler = async (req, res) => {
     }
 };
 
-// Handler para eliminar un rango horario
-const deleteRangoHorarioHandler = async (req, res) => {
+// Handler para actualizar un rango horario
+const updateRangoHorarioHandler = async (req, res) => {
     const { id } = req.params;
+    const { inicio, fin } = req.body;
+    const errores = [];
+    if (isNaN(id) || id <= 0) {
+        errores.push("El parámetro ID fue ingresado incorrectamente" );
+    }
+    if (inicio && !/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/.test(inicio)) {
+        errores.push('El formato para FECHA INICIO es incorrecto');
+    }
+    if (fin && !/^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/.test(fin)) {
+        errores.push('El formato para FECHA FIN es incorrecto');
+    }
+    if (errores.length > 0) {
+        return res.status(400).json({ errores });
+    }
     try {
-        const deletedRangoHorario = await deleteRangoHorario(id);
-        if (!deletedRangoHorario) {
-            return res.status(404).json({ message: 'Rango Horario no encontrado' });
+        const updatedRangoHorario = await updateRangoHorario(id, { inicio, fin });
+        if (updatedRangoHorario === null) {
+            return res.status(400).json({ message: 'RangoHorario no encontrado' });
+        } else if (updatedRangoHorario === false) {
+            return res.status(400).json({ message: 'Error al actualizar el RangoHorario' });
         }
-        return res.json(deletedRangoHorario);
+        return res.status(200).json(updatedRangoHorario);
     } catch (error) {
         return res.status(500).json({
-            message: 'Error al eliminar el rango horario',
+            message: 'Error al actualizar el rango horario',
             error: error.message
         });
     }
 };
 
-// Handler para actualizar un rango horario
-const updateRangoHorarioHandler = async (req, res) => {
+// Handler para eliminar un rango horario
+const deleteRangoHorarioHandler = async (req, res) => {
     const { id } = req.params;
-    const rangoHorarioData = req.body;
+    if (isNaN(id) || id <= 0) {
+        return res.status(400).json({ message: "El parámetro ID fue ingresado incorrectamente" });
+    }
     try {
-        const updatedRangoHorario = await updateRangoHorario(id, rangoHorarioData);
-        if (!updatedRangoHorario) {
-            return res.status(404).json({ message: 'Rango Horario no encontrado' });
+        const deletedRangoHorario = await deleteRangoHorario(id);
+        if (!deletedRangoHorario) {
+            return res.status(400).json({ message: 'Rango Horario no encontrado' });
         }
-        return res.json(updatedRangoHorario);
+        return res.json(deletedRangoHorario);
     } catch (error) {
         return res.status(500).json({
-            message: 'Error al actualizar el rango horario',
+            message: 'Error al eliminar el rango horario',
             error: error.message
         });
     }
