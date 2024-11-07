ddfde634162a2ccfa5eb00fd2a73304593ef5ae9
Erick Pajares <gunminiho@gmail.com>
2024-11-07 15:35:31 -0500

    backend v1.1

--- routes/usuariosRouter.js
+++ routes/usuariosRouter.js
@@ -6,6 +6,6 @@ const permisoAutorizacion = require("../checkers/roleAuth");
 
 router.get("/", permisoAutorizacion(["all_system_access", "read_usuarios"]), getAllUsersHandler);
 router.get("/:id", permisoAutorizacion(["all_system_access", "read_usuarios"]), getUserByIdHandler);
-router.delete("/:id", permisoAutorizacion(["all_system_access", "delete_usuarios"]), deleteUserHandler);
+router.delete("/:usuario", permisoAutorizacion(["all_system_access", "delete_usuarios"]), deleteUserHandler);
 
 module.exports = router;
