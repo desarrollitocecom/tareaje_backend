5a4a4c0c4f6569400d8c20e42aa46104e490bd4b
vieri1 <vieriflores2002xdxd@gmail.com>
2024-11-15 10:21:59 -0500

    empleado corregido

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -5,7 +5,7 @@ const getAllEmpleados = async (page = 1, limit = 20) => {
     const offset = (page - 1) * limit;
     try {
         const response = await Empleado.findAndCountAll({
-            attributes: ['id','nombres', 'apellidos', 'dni','celular'],
+            attributes: ['id', 'nombres', 'apellidos', 'dni', 'celular', 'status'],
             include: [
                 { model: Cargo, as: 'cargo', attributes: ['nombre'] },
                 { model: Subgerencia, as: 'subgerencia', attributes: ['nombre'] },
@@ -41,8 +41,8 @@ const getEmpleado = async (id) => {
 
         });
         if (!response) return null;
-        const estado = response.state ? "Trabajando" : "Cesado";
-        return { ...response.toJSON(), estado }
+        response.setDataValue('estado', response.state ? "Trabajando" : "Cesado");
+        return response || null
     } catch (error) {
         console.error("Error al obtener un empleado en el controlador:", error);
         return false;
@@ -100,8 +100,7 @@ const deleteEmpleado = async (id) => {
 
 };
 const updateEmpleado = async (
-    id,
-    {
+    {id,
         nombres,
         apellidos,
         dni,
@@ -123,36 +122,40 @@ const updateEmpleado = async (
         id_grado_estudios,
         id_subgerencia,
         id_funcion,
-        id_lugar_trabajo,
+        id_lugar_trabajo
     }
 ) => {
+    
+    
     try {
         const empleado = await getEmpleado(id);
+          console.log("empleado 1",empleado.id);
+          
         if (empleado) {
-            await empleado.update({
-                nombres: nombres,
-                apellidos: apellidos,
-                dni: dni,
-                ruc: ruc,
-                hijos: hijos,
-                edad: edad,
-                f_nacimiento: f_nacimiento,
-                correo: correo,
-                domicilio: domicilio,
-                celular: celular,
-                f_inicio: f_inicio,
-                foto: foto,
-                observaciones: observaciones,
-                id_cargo: id_cargo,
-                id_turno: id_turno,
-                id_regimen_laboral: id_regimen_laboral,
-                id_sexo: id_sexo,
-                id_jurisdiccion: id_jurisdiccion,
-                id_grado_estudios: id_grado_estudios,
-                id_subgerencia: id_subgerencia,
-                id_funcion: id_funcion,
-                id_lugar_trabajo: id_lugar_trabajo,
-            });
+            await empleado.update(
+                {nombres,
+                apellidos,
+                dni,
+                ruc,
+                hijos,
+                edad,
+                f_nacimiento,
+                correo,
+                domicilio,
+                celular,
+                f_inicio,
+                foto,
+                observaciones,
+                id_cargo,
+                id_turno,
+                id_regimen_laboral,
+                id_sexo,
+                id_jurisdiccion,
+                id_grado_estudios,
+                id_subgerencia,
+                id_funcion,
+                id_lugar_trabajo,}
+            );
         }
         return empleado || null;
     } catch (error) {
