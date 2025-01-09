191125981cf866cec466a32cd33ea6401a9c541c
EduardoVillegasB02 <eduvb02@gmail.com>
2025-01-09 15:49:48 -0500

    Cambios en descansos

--- routes/descansoRouter.js
+++ routes/descansoRouter.js
@@ -3,6 +3,7 @@ const rutas = Router();
 const {
     getAllDescansosHandler,
     getDescansosHandler,
+    getDescansosRangoHandler,
     createDescansosHandler,
     updateDescansoHandler,
     deleteDescansoHandler
@@ -11,6 +12,7 @@ const permisoAutorizacion = require("../checkers/roleAuth");
 
 rutas.get("/",permisoAutorizacion(["all_system_access", "read_descanso"]), getAllDescansosHandler);
 rutas.get("/:id",permisoAutorizacion(["all_system_access", "read_descanso"]), getDescansosHandler);
+rutas.post("/rango/",permisoAutorizacion(["all_system_access", "read_descanso"]), getDescansosRangoHandler);
 rutas.post("/",permisoAutorizacion(["all_system_access", "create_descanso"]), createDescansosHandler);
 rutas.patch("/:id",permisoAutorizacion(["all_system_access", "update_descanso"]), updateDescansoHandler);
 rutas.delete("/:id",permisoAutorizacion(["all_system_access", "delete_descanso"]), deleteDescansoHandler);
